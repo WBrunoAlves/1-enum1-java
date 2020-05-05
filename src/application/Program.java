9454b3a83af5335ba53dbdbb0333b68bb84c8811
Wenderson Bruno <wendersonbruno10@gmail.com>
2020-05-05 15:18:34 -0300

    first

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+
+
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT); // Instanciamento do construtor CONSTRUTOR , COM O DATE E O ENUM COMO PARAMETRO
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;  
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1); // aqui passa o atributo do enum que vai ser impresso
+		System.out.println(os2); // neste caso será impresso a string passada no valueOF
+	}
+}
---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package entities.enums;
+/*
+ * AQUI FICA A LISTA DE PROCESSOS do enum 
+ * */
+public enum OrderStatus {
+
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+}
